a6bd36231e3719eaab80df2cf819cfc626b90a3b
Martin Perez <l.pezm021228@itses.edu.mx>
2026-05-26 14:58:07 -0600

    Add files via upload
    Se agregaron los comentarios

--- Pagina Web II Mis Abril/Formulario/JS/app.js
+++ Pagina Web II Mis Abril/Formulario/JS/app.js
@@ -1,3 +1,5 @@
+
+//Se crea lafuncion encargada de mostrar en los los console.log mediante los parametros del evento 
 function mostrar(event) {
     event.preventDefault();
     const form = document.getElementById('formulario');
@@ -12,14 +14,14 @@ function mostrar(event) {
 }
 
 
-
+//Se crea lafalerta la cual nbos indica que se subio la informacion
 function alerta(event) {
     event.preventDefault();
     alert('Información enviada');
 
 }
 
-
+//Se crea la funcion que se encarga de cambiar de color segun si mantienes presionado o no la cassilla del formulario
 function cambiar(elementoInput) {
     elementoInput.style.background = 'yellow'
 
